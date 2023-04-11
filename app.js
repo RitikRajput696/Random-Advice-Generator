4d6d873dcca600f440c8f00e1052b3a4579ca2ab
ritikrajput696 <rixusinput696@gmail.com>
2023-04-11 11:44:42 +0530

    fixed button rotate issue

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
+const circle = document.querySelector('.circle');
+
 async function getAdvice() {
-  const response = await fetch("https://api.adviceslip.com/advice");
+  console.log('Fetching Data');
+  const response = await fetch('https://api.adviceslip.com/advice');
   const data = await response.json();
 
-  const advice = await data.slip.advice;
-  const id = await data.slip.id;
+  console.log('Data is fetched');
+  const advice = data.slip.advice;
+  const id = data.slip.id;
+  console.log('printing Data');
   console.log(advice);
   console.log(id);
 
-  document.querySelector("#advice-id").innerHTML = `ADVICE #${data.slip.id}`;
-  document.querySelector("#advice-text").innerHTML = `"${data.slip.advice}"`;
+  document.querySelector('#advice-id').innerHTML = `ADVICE #${data.slip.id}`;
+  document.querySelector('#advice-text').innerHTML = `"${data.slip.advice}"`;
 }
 
-document.querySelector(".circle").addEventListener("click", () => {
-  document.querySelector(".circle").classList.add("rotate");
-  setInterval(() => {
-    document.querySelector(".circle").classList.remove("rotate");
-  }, 210);
+circle.addEventListener('click', () => {
+  getAdvice();
+  circle.style.transform += 'rotate(180deg)';
 });
