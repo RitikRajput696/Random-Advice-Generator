90197fd037e0b6a47c205394569c0bdef649d103
RitikRajput696 <rixusinput696@gmail.com>
2023-11-18 23:41:35 +0530

    added try catch

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const circle = document.querySelector('.circle');
 
 async function getAdvice() {
-  console.log('Fetching Data');
-  const response = await fetch('https://api.adviceslip.com/advice');
-  const data = await response.json();
+  try {
+    const response = await fetch('https://api.adviceslip.com/advice');
 
-  console.log('Data is fetched');
-  const advice = data.slip.advice;
-  const id = data.slip.id;
-  console.log('printing Data');
-  console.log(advice);
-  console.log(id);
+    if (!response.ok) {
+      throw new Error(`HTTP error ! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    const advice = data.slip.advice;
+    const id = data.slip.id;
 
-  document.querySelector('#advice-id').innerHTML = `ADVICE #${data.slip.id}`;
-  document.querySelector('#advice-text').innerHTML = `"${data.slip.advice}"`;
+    document.querySelector('#advice-id').innerHTML = `ADVICE #${data.slip.id}`;
+    document.querySelector('#advice-text').innerHTML = `"${data.slip.advice}"`;
+  } catch (error) {
+    console.error('Error fetching data:', error.message);
+  }
 }
 
 circle.addEventListener('click', () => {
